79b93d1fa9556968b98d6bef958ba711e8ed8530
deknasa <80618997+deknasa@users.noreply.github.com>
2022-04-12 21:05:15 +0700

    set proccess env port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =  require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const locationRoutes = require('./routes/location')
 const todoRoutes = require('./routes/todo')
 const userRoutes = require('./routes/user')
